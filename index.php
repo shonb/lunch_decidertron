9038bdff784a1892827f4e16dc0085eea43d2764
Adrian Evans <evansa@tabcorp.com.au>
2015-08-14 09:33:54 +1000

    Test edit of the links

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <h1>NEW HEADER</h1>
-<a href="index_melb.php">Melbourne</a> - <a href="index_syd.php">Sydney</a>
+<a href="index_melb.php">Melbourne</a> - <a href="index_syd.php">Sydney</a> - <span>test</span>
