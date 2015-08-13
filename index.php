e46824c6b2faf97d827dc720ade3ebf7824d8354
besters <shonb@bestersolutions.com.au>
2015-08-14 07:58:54 +1000

    #Lunch-102 : Add Perth to the Mix via a local sub branch

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-<?php
-	//TODO : Remove this dreaded todo comment
-?>
-<a href="index_melb.php">Melbourne</a> - <a href="index_syd.php">Sydney</a> - <a href="index_ade.php">Adelaide</a>
+<a href="index_melb.php">Melbourne</a> - <a href="index_syd.php">Sydney</a> - <a href="index_ade.php">Adelaide</a> - <a href="index_per.php">Perth</a>
