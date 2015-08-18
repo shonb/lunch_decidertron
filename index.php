d697a32e6364c6d4cfec78cc2ff0e1f66dbc033e
Adrian Evans <evansa@tabcorp.com.au>
2015-08-18 10:51:54 +1000

    Added more capitol cities

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
   <li><a href="index_syd.php">Sydney</a></li>
   <li><a href="index_bris.php">Brisbane</a></li>
   <li><a href="index_ade.php">Adelaide</a></li>
+  <li><a href="index_per.php">Perth</a></li>
+  <li><a href="index_dar.php">Darwin</a></li>
+  <li><a href="index_hob.php">Hobart</a></li>
 </ul>
 
 
