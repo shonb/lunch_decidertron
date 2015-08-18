74d809054101a7d99e0874b1b6baec791b7a7b43
Adrian Evans <evansa@tabcorp.com.au>
2015-08-18 10:50:22 +1000

    Added some other capitol cities

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <ul>
   <li><a href="index_melb.php">Melbourne</a></li>
   <li><a href="index_syd.php">Sydney</a></li>
+  <li><a href="index_bris.php">Brisbane</a></li>
+  <li><a href="index_ade.php">Adelaide</a></li>
 </ul>
 
 
